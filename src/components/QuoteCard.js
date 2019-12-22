5f748f2f09fc51e95c4a1ad5634a024648a697d1
Maxime Papin <max.papin33@gmail.com>
2019-12-22 18:06:59 +0100

    added props in quotecard

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './QuoteCard.css'
 
-function QuoteCard() {
+function QuoteCard(props) {
     return (
         <figure className="QuoteCard">
             <img 
-            src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185" 
-            alt="Nelson Muntz"></img>
+            src={props.image} 
+            alt={props.character}></img>
             <figcaption>
                 <blockquote>
-                    Shoplifting is a victimless crime, like punching someone in the dark.
+                    {props.quote}
                 </blockquote>
-                <cite>Nelson Muntz</cite>
+                    <cite>{props.character}</cite>
             </figcaption>
         </figure>
     );
